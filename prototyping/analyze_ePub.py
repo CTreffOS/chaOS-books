e24f51a77c6f83e462c60a535c0f3bd59322ce3e
jsaalfeld <git@jochen-saalfeld.de>
2015-01-12 17:57:35 +0100

    extracting ePubs and sort their content

--- prototyping/analyze_ePub.py
+++ prototyping/analyze_ePub.py
@@ -1,6 +1,7 @@
 import zipfile
 import os
 import shutil
+import xml.dom.minidom
 
 #Gather all the ePubs
 toAnalyze = []
@@ -11,62 +12,82 @@ for file in os.listdir(dropIn):
 
 #just open the first file to see whats going on
 bookDir = toAnalyze[0]
-print bookDir
 unzipDir = bookDir.split('.epub')[0]
 os.mkdir(unzipDir)
 with zipfile.ZipFile(bookDir, 'r') as z:
 	z.extractall(unzipDir)
+
 #Now wie have everything we need in place - time to get some stuff done
-
-
-#shutil.rmtree(unzipDir)
+epubStruct = []
+for root, dirs, files in os.walk(unzipDir, topdown=False):
+	for name in files:
+		epubStruct.append(os.path.join(root, name))
+		for name in dirs:
+			epubStruct.append(os.path.join(root, name))
+cssFiles = []
+opfFiles = []
+xmlFiles = []
+htmlFiles = []
+ncxFiles = []
+xhtmlFiles = []
+bookVersion = '2.0'
+for file in epubStruct:
+	if file.endswith('.css'):
+		cssFiles.append(file)
+	elif file.endswith('.opf'):
+		opfFiles.append(file)
+	elif file.endswith('.xml'):
+		xmlFiles.append(file)
+	elif file.endswith('.html'):
+		htmlFiles.append(file)
+	elif file.endswith('.ncx'):
+		ncxFiles.append(file)
+	elif file.endswith('.xhtml'):
+		#an xhtml file only existis in v3
+		xhtmlFiles.append(file)
+		bookVersion = '3.0'
 
 '''
-#Stuff that works with the ebooklib
-
-from ebooklib import epub
-
-#Better Pipe the output to a file to gather a better overview
-# ID = 2  is CSS
-# ID = 9  is content
-# ID = 4  is NCX
-# ID = 1  is Binary Content
-for bookDir in toAnalyze:
-	print ' '
-	book = epub.read_epub(bookDir)
-	print '-- the item ids in the book ' + bookDir + ' --'
-	for item in book.get_items():
-		print '----------------'
-		print 'ID: ' + item.get_id()
-		print 'Type Number: ' + str(item.get_type())
-		if item.get_type() == 2:
-			print 'Type: CSS'
-		elif item.get_type() == 4:
-			print 'Tpye: NCX'
-		elif item.get_type() == 9:
-			print 'Type: Content'
-		elif item.get_type() == 1:
-			print 'Type: Binary Content'
-#Just examine the first book in the list, otherwise the output is too long
-book = epub.read_epub(toAnalyze[0])
-for item in book.get_items():
-	if item.get_id() != 'ncx':
-		print ' '
-		print '-- the ' + item.get_id() + ' item --'
-		print '-- the type --'
-		print item.get_type()
-		print '-- the content --'
-		print item.get_content()
-	if item.get_id() == 'ncx':
-		print '-- the ncx item --'
-		htmlItem = epub.EpubHtml(item)
-		print '-- the Nav item --'
-		navItem = epub.EpubNav(htmlItem)
-		print navItem
-		print '-- the language --'
-		print htmlItem.get_language()
-		print '-- the content via Item --'
-		print item.get_content()
-		print '-- the type via nav Item --'
-		print navItem.get_type()
+print '-- cssFiles --'
+print cssFiles
+print '-- opfFiles --'
+print opfFiles
+print '-- xmlFiles --'
+print xmlFiles
+print '-- htmlFiles --'
+print htmlFiles
+print '-- ncxFiles --'
+print ncxFiles
+print '-- xhtmlFiles --'
+print xhtmlFiles
 '''
+
+#First we need some Infos about the book
+if bookVersion == '2.0':
+	if len(opfFiles) == 1:
+		opf = xml.dom.minidom.parse(opfFiles[0])
+	elif len(opfFiles) < 1:
+		#ToDo
+		print 'error'
+	elif len(opfFiles) > 1:
+		#ToDo
+		print 'error'
+
+	if len(xmlFiles) == 1:
+		xml = xml.dom.minidom.parse(xmlFiles[0])
+	elif len(xmlFiles) < 1:
+		#ToDo
+		print 'error'
+	elif len(xmlFiles) > 1:
+		#ToDo
+		print 'error'
+elif bookVersion == '3.0':
+	if len(xhtmlFiles) == 1:
+		xhtml = xml.dom.minidom.parse(xhtmlFiles[0])
+	elif len(xhtmlFiles) < 1:
+		#ToDo
+		print 'error'
+	elif len(xhtmlFiles) > 1:
+		#ToDo
+		print 'error'
+shutil.rmtree(unzipDir)
